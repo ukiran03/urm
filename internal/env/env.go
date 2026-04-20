c4c229cc2aef32a3037854412abbc2fa449bc9d4
ukiran03 <ushakiranreddi@gmail.com>
2026-04-20 11:55:51 +0530

    feat[env]: make env pkg more useful for other pkgs
    Note: I discovered that each package can maintain its own init()
    function and even multiple ones per package

--- internal/env/env.go
+++ internal/env/env.go
@@ -3,25 +3,33 @@ package env
 import (
 	"os"
 	"os/user"
+
+	"ukiran.com/urm/internal/clog"
 )
 
 var (
-	UID      int
-	Username string
-	HomeDir  string
+	UID           int
+	Username      string
+	HomeDir       string
+	MountInfoFile = "/proc/self/mountinfo"
 )
 
-// Init initializes the global environment variables.
-// Call this once at the very start of your main() function.
-func Init() error {
+func init() {
 	u, err := user.Current()
 	if err != nil {
-		return err
+		clog.Error("failed to retrieve current user context", "err", err)
+		os.Exit(1)
 	}
 
 	UID = os.Getuid()
 	Username = u.Username
 	HomeDir = u.HomeDir
+}
 
-	return nil
+func OpenMountInfo() (*os.File, error) {
+	f, err := os.Open(MountInfoFile)
+	if err != nil {
+		clog.Error("mount file access failed", "file", MountInfoFile, "err", err)
+	}
+	return f, err
 }
