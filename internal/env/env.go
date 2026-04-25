ffb3bb8c28d77150c2e5aca5e4de81a292beb5e9
ukiran03 <ushakiranreddi@gmail.com>
2026-04-25 22:50:04 +0530

    refactor(env): add more info about the HomeDir to env

--- internal/env/env.go
+++ internal/env/env.go
@@ -1,17 +1,21 @@
 package env
 
 import (
+	"fmt"
 	"os"
 	"os/user"
 
+	"golang.org/x/sys/unix"
 	"ukiran.com/urm/internal/clog"
 )
 
+const MountInfoFile = "/proc/self/mountinfo"
+
 var (
-	UID           int
-	Username      string
-	HomeDir       string
-	MountInfoFile = "/proc/self/mountinfo"
+	UID       int
+	Username  string
+	HomeDir   string
+	HomeDevID uint64
 )
 
 func init() {
@@ -24,6 +28,18 @@ func init() {
 	UID = os.Getuid()
 	Username = u.Username
 	HomeDir = u.HomeDir
+
+	var homeStat unix.Stat_t
+	if err := unix.Stat(HomeDir, &homeStat); err != nil {
+		clog.Error(fmt.Sprintf("failed to Stat %s", HomeDir), "err", err)
+		os.Exit(1)
+	}
+
+	HomeDevID = homeStat.Dev
+
+	// Extract permission bits (rwxrwxrwx) using a bitwise AND with 0777
+	// This yields the standard Unix permission bits.
+	// HomePerms = os.FileMode(homeStat.Mode & 0o777)
 }
 
 func OpenMountInfo() (*os.File, error) {
