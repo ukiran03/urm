9844fb8dd23dcda00c06ba6c22e3e43854863bcb
ukiran03 <ushakiranreddi@gmail.com>
2026-04-25 22:58:55 +0530

    refactor(trash): simplify the TrashCan logic, struct

--- internal/trash/trash.go
+++ internal/trash/trash.go
@@ -3,76 +3,56 @@ package trash
 import (
 	"errors"
 	"fmt"
+	"io/fs"
+	"os"
 
-	"golang.org/x/sys/unix"
 	"ukiran.com/urm/internal/env"
 	"ukiran.com/urm/internal/fsys"
 )
 
+var (
+	ErrNilMinInfo = errors.New("mount-info cannot be nil")
+	ErrNoPerms    = fmt.Errorf(
+		"have no permissions to write: %w", fs.ErrPermission,
+	)
+)
+
 type TrashCan struct {
-	TopDir   string // Directory where a file system is mounted
 	DevID    uint64 // Device ID of the TopDir on the File System
+	TopDir   string // Directory where a file system is mounted
 	TrashDir string // Trash location for the TopDir
 }
 
-type CanType int
-
-const (
-	HomeCan    CanType = iota
-	SpecialCan         // Non-Home can
-)
-
-// NewTrashCan returns a new TrashCan for the given MountInfo (e.g.,
-// partition or device).  It does not initialize the TrashCan for
-// immediate operations by default.
-func NewTrashCan(minfo *fsys.MountInfo, canType CanType) (*TrashCan, error) {
+// NewTrashCan returns new TrashCan for the given MountInfo (mountpoint)
+func NewTrashCan(minfo *fsys.MountInfo) (*TrashCan, error) {
 	if minfo == nil {
-		return nil, fmt.Errorf("ERR: mount info cannot be nil")
+		return nil, ErrNilMinInfo
 	}
-	var trashPath string
-	var err error
-
-	switch canType {
-	case HomeCan:
-		var homeStat unix.Stat_t
-		if err = unix.Stat(env.HomeDir, &homeStat); err != nil {
-			return nil, &TrashError{Op: "Stat", Path: env.HomeDir, Err: err}
-		}
-		// Ensure we are on the same device
-		if homeStat.Dev != minfo.DevID {
-			return nil, fmt.Errorf("ERR: device ID mismatch for home directory")
-		}
-		trashPath, err = getHomeTrashPath(env.HomeDir)
-		if err != nil {
-			return nil, &TrashError{Op: "getHomeTrashPath", Path: env.HomeDir, Err: err}
-		}
-
-	case SpecialCan:
-		// Ensure MountPoint isn't empty
-		if minfo.MountPoint == "" {
-			return nil, errors.New("ERR: Empty MountPoint for special can")
-		}
-		trashPath, err = getSpecialTrashPath(minfo.MountPoint, env.UID)
-		if err != nil {
-			return nil, &TrashError{
-				Op:   "getSpecialTrashDir",
-				Path: minfo.MountPoint, Err: err,
-			}
-		}
-	default:
-		return nil, fmt.Errorf("ERR: unsupported trash can type: %v", canType)
+	if minfo.IsReadOnly {
+		return nil, ErrNoPerms
 	}
 
-	return &TrashCan{
-		TopDir:   minfo.MountPoint,
-		DevID:    minfo.DevID,
-		TrashDir: trashPath,
-	}, nil
+	can := &TrashCan{
+		DevID:  minfo.DevID,
+		TopDir: minfo.MountPoint,
+	}
+
+	if minfo.DevID == env.HomeDevID {
+		can.TrashDir = getTrashPath(minfo.MountPoint, true)
+	} else {
+		can.TrashDir = getTrashPath(minfo.MountPoint, false)
+	}
+
+	return can, nil
+}
+
+func (tc *TrashCan) MkdirP() error {
+	return os.MkdirAll(tc.TrashDir, 0o755)
 }
 
 // All these methods of TrashCan assume the relevant directories are
 // created and checked in prior, see NewTrashCan
-func (tc *TrashCan) Move(entry *FileEntry) error                { return nil }
-func (tc *TrashCan) Restore(entry *FileEntry, dst string) error { return nil }
-func (tc *TrashCan) Delete(entry *FileEntry) error              { return nil }
-func (tc *TrashCan) List() ([]*FileEntry, error)
+func (tc *TrashCan) Move(entry *TrashEntry) error                { return nil }
+func (tc *TrashCan) Restore(entry *TrashEntry, dst string) error { return nil }
+func (tc *TrashCan) Delete(entry *TrashEntry) error              { return nil }
+func (tc *TrashCan) List() ([]*TrashEntry, error)
